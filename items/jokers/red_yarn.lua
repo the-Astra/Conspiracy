c3652fa407e42c311488a7135b2d2a5afef8bc76
DJ Stalder <djs8743@rit.edu>
2025-05-29 00:05:28 -0400

    Fixed incorrect localization key on joker artist badge

--- items/jokers/red_yarn.lua
+++ items/jokers/red_yarn.lua
@@ -44,7 +44,7 @@ SMODS.Joker {
     end,
     set_badges = function(self, card, badges)
         if self.discovered then
-            badges[#badges + 1] = create_badge(localize('k_mxms_artist') .. ': anerdymous', G.C.BLACK, G.C.WHITE, 1)
+            badges[#badges + 1] = create_badge(localize('k_cspy_artist') .. ': anerdymous', G.C.BLACK, G.C.WHITE, 1)
         end
     end
 }
